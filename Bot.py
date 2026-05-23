6d1cf6536d21a6fa291ba414217f366a15146a56
katya15110 <vovchenko151107@gmail.com>
2026-05-23 21:48:09 +0300

    checking

--- Bot.py
+++ Bot.py
@@ -5,24 +5,18 @@ class Bot(Player):
 
     def __init__(self, name, board, enemy_board):
         super().__init__(name, board, enemy_board)
-        self.shoted = set() # це множина, куди бот записує клітинки (координати), в які вже стріляв
-        self.possible_opt = [] # список для того, аби записувати сусідні клітинки для тих, куди стріляли і там був корабель
+        self.shoted = set()
+        self.possible_opt = set()
 
     def get_move(self):
-        # 1. Якщо є варіанти для добивання корабля (коли не пустий список)
-        if self.possible_opt:
-            move = self.possible_opt.pop(0)
-
-            # Якщо ми вже сюди стріляли, беремо наступну клітинку з черги
-            while move in self.shoted and self.possible_opt:
-                move = self.possible_opt.pop(0)
-
-            # Перевіряємо фінальний варіант
+        # 1. Добиваємо кораблі
+        while self.possible_opt:
+            move = self.possible_opt.pop() # pop() без індексу для множини
             if move not in self.shoted:
                 self.shoted.add(move)
                 return move
 
-        # 2. Постріли випадково (якщо цілей для добивання немає)
+        # 2. Якщо немає цілей, стріляємо випадково
         while True:
             row = randint(0, 9)
             col = randint(0, 9)
@@ -31,23 +25,14 @@ class Bot(Player):
                 return (row, col)
 
     def register_hit(self, row, col):
-        # Цей метод викликається коли бот влучає в клітинку успішно і
-        # він додає сусідні клітинки до списку, де можливо буде ще один успішний постріл
-
-        # Напрямки для перевірки: (зміщення_row, зміщення_col)
-        # (-1, 0) вгору, (1, 0) вниз, (0, -1) ліворуч, (0, 1) праворуч
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
         for d_row, d_col in directions:
-            n_row = row + d_row
-            n_col = col + d_col
-
-            # Перевіряємо математичні межі поля (від 0 до 9 включно)
+            n_row, n_col = row + d_row, col + d_col
             if 0 <= n_row < 10 and 0 <= n_col < 10:
                 if (n_row, n_col) not in self.shoted:
                     self.possible_opt.add((n_row, n_col))
 
-def make_turn(self, player_board, destroyed_ships):
+    def make_turn(self, player_board, destroyed_ships):
         bot_hit_flag = False
 
         while True:
@@ -69,4 +54,4 @@ def make_turn(self, player_board, destroyed_ships):
             else:
                 break
 
-        return " Бот влучив у твій корабель! Твій хід." if bot_hit_flag else " Бот промахнувся! Твій хід."
+        return " Бот влучив у твій корабель! Твій хід." if bot_hit_flag else " Бот промахнувся. Твій хід."
